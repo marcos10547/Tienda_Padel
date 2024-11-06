591451e677907214360b3b9ad0700e3314e7d30a
marcos10547 <160898439+marcos10547@users.noreply.github.com>
2024-11-06 12:14:31 +0100

    update de archivos

---
+++ Api/app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Importar rutas
+const userRoutes = require('./routes/userRoutes');
+const loginRoutes = require('./routes/loginRoutes');
+const registerRoutes = require('./routes/registerRoutes');
+
+// Usar rutas
+app.use('/api/users', userRoutes);
+app.use('/api/login', loginRoutes);
+app.use('/api/register', registerRoutes);
+
+// Ruta de prueba
+app.get('/', (req, res) => {
+    res.send('¡El servidor esta funcionando!');
+});
+
+// Manejo de errores
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Algo salió mal!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+
+module.exports = app;
